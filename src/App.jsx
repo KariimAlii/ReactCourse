4496931b33b708d5987aeab1c2465c96618a0365
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-09-16 22:58:18 +0300

    Using & Outputting Dynamic Values

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
 function Header() {
     return (
         <header>
             <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
             <h1>React Essentials</h1>
             <p>
-                Fundamental React concepts you will need for almost any app you are
+                { reactDescriptions [getRandomInt(reactDescriptions.length - 1)] } React concepts you will need for almost any app you are
                 going to build!
             </p>
         </header>
