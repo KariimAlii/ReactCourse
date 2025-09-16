1da4325d7ef9f47943db8f318088c305a5aa2109
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-09-17 01:15:21 +0300

    Passing Arguments to Event Functions

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import TabButton from "./components/TabButton/TabButton.jsx";
 
 
 function App() {
-    function handleSelect() {
-        console.log("Helloo --- Selected")
+    function handleSelect(selectedButton) {
+        // selectedButton => 'components' , 'jsx' , 'props' , 'state'
+        console.log(selectedButton)
     }
     return (
         <>
@@ -26,10 +27,10 @@ function App() {
                   <section id="examples">
                       <h2>Examples</h2>
                       <menu>
-                          <TabButton onSelect={handleSelect}>Components</TabButton>
-                          <TabButton onSelect={handleSelect}>JSX</TabButton>
-                          <TabButton onSelect={handleSelect}>PROPS</TabButton>
-                          <TabButton onSelect={handleSelect}>State</TabButton>
+                          <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+                          <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                          <TabButton onSelect={() => handleSelect('props')}>PROPS</TabButton>
+                          <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
                       </menu>
                   </section>
               </main>
