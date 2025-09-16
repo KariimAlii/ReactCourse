1136dbb6fc0c0fcb9b26e2117cdd6b386a4a2f38
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-09-17 01:01:54 +0300

    Reacting to Events

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,7 +1,16 @@
 ﻿export default function TabButton({children}) {
+    // document.querySelector('button').addEventListener('click', () => {
+    //
+    // })
+
+    // the advantage to declare this function inside the TabButton Function is that
+    // this way it has access to the components Props and State
+    function handleClick() {
+        console.log("Helooooooooooooooooooo")
+    }
     return (
         <li>
-            <button>{children}</button>
+            <button onClick={handleClick}>{children}</button>
         </li>
     )
 }
