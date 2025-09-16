c772b30206d9ae13020c8f8b40789a8762055455
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-09-16 23:39:17 +0300

    Configuring Components with Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
-
+import componentsImg from './assets/components.png';
 import './App.css';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
@@ -21,12 +21,30 @@ function Header() {
     )
 }
 
+function CoreConcept(props) {
+    return  (
+        <li>
+            <img src={props.image} alt=""/>
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+        </li>
+    )
+}
+
 function App() {
     return (
         <>
             <div>
                 <Header />
               <main>
+                  <section id="core-concepts">
+                      <h2>Core Concepts</h2>
+                      <ul>
+                          <CoreConcept title="Components" description="The Core UI Building Block" image={componentsImg} />
+                          <CoreConcept title="Components" description="The Core UI Building Block" image={componentsImg} />
+                          <CoreConcept title="Components" description="The Core UI Building Block" image={componentsImg} />
+                      </ul>
+                  </section>
                   <h2>Time to get started!</h2>
               </main>
           </div>
