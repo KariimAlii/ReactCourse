cca06e7d6ea9351a69633eb326ae716d6c515107
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-09-20 12:18:31 +0300

    Rendering Content Conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { CORE_CONCEPTS } from './data.js'
+import {CORE_CONCEPTS} from './data.js'
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import TabButton from "./components/TabButton/TabButton.jsx";
@@ -13,10 +13,9 @@ function App() {
     // -------- (1) it re-invokes the component function
     // -------- (2) it updated the (selectedTopic) value
 
-    const [ selectedTopic, setSelectedTopic ] = useState('components')
+    const [selectedTopic, setSelectedTopic] = useState(null)
 
     function handleSelect(selectedButton) {
-        // useState()   ❌❌ you cannot use it here ❌❌
         setSelectedTopic(selectedButton);
         console.log(selectedTopic)
     }
@@ -26,37 +25,44 @@ function App() {
     return (
         <>
             <div>
-                <Header />
-              <main>
-                  <section id="core-concepts">
-                      <h2>Core Concepts</h2>
-                      <ul>
-                          <CoreConcept title={ CORE_CONCEPTS[0].title } description={ CORE_CONCEPTS[0].description } image={ CORE_CONCEPTS[0].image } />
-                          <CoreConcept {...CORE_CONCEPTS[1]} />
-                          <CoreConcept {...CORE_CONCEPTS[2]} />
-                          <CoreConcept {...CORE_CONCEPTS[3]} />
-                      </ul>
-                  </section>
-                  <section id="examples">
-                      <h2>Examples</h2>
-                      <menu>
-                          <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-                          <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-                          <TabButton onSelect={() => handleSelect('props')}>PROPS</TabButton>
-                          <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
-                      </menu>
-                      <div id="tab-content">
-                          <h3>{ EXAMPLES[selectedTopic].title }</h3>
-                          <p>{ EXAMPLES[selectedTopic].description }</p>
-                          <pre>
-                              <code>{ EXAMPLES[selectedTopic].code }</code>
-                          </pre>
-                      </div>
-                  </section>
-              </main>
-          </div>
-      </>
-  )
+                <Header/>
+                <main>
+                    <section id="core-concepts">
+                        <h2>Core Concepts</h2>
+                        <ul>
+                            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description}
+                                         image={CORE_CONCEPTS[0].image}/>
+                            <CoreConcept {...CORE_CONCEPTS[1]} />
+                            <CoreConcept {...CORE_CONCEPTS[2]} />
+                            <CoreConcept {...CORE_CONCEPTS[3]} />
+                        </ul>
+                    </section>
+                    <section id="examples">
+                        <h2>Examples</h2>
+                        <menu>
+                            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+                            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                            <TabButton onSelect={() => handleSelect('props')}>PROPS</TabButton>
+                            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+                        </menu>
+
+                        {!selectedTopic ? <p>Please select a topic!</p> : null}
+
+                        {selectedTopic ? (
+                            <div id="tab-content">
+                                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                                <p>{EXAMPLES[selectedTopic].description}</p>
+                                <pre>
+                                <code>{EXAMPLES[selectedTopic].code}</code>
+                              </pre>
+                            </div>
+                        ) : null}
+
+                    </section>
+                </main>
+            </div>
+        </>
+    )
 }
 
 export default App
