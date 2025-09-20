b04fa5f8f63600d596c09e9dc3ba0720f2fde722
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-09-20 12:39:59 +0300

    Render list of items dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -29,18 +29,18 @@ function App() {
                 <main>
                     <section id="core-concepts">
                         <h2>Core Concepts</h2>
-                        {
-                            [
-                                <p>Hello</p>,
-                                <p>World</p>
-                            ]
-                        }
                         <ul>
-                            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description}
-                                         image={CORE_CONCEPTS[0].image}/>
-                            <CoreConcept {...CORE_CONCEPTS[1]} />
-                            <CoreConcept {...CORE_CONCEPTS[2]} />
-                            <CoreConcept {...CORE_CONCEPTS[3]} />
+                            {
+                                CORE_CONCEPTS.map((coreConcept) => (
+                                    <CoreConcept {...coreConcept} key={coreConcept.title}/>
+
+                                // <CoreConcept
+                                //     key={coreConcept.title}
+                                //     title={coreConcept.title}
+                                //     description={coreConcept.description}
+                                //     image={coreConcept.image}/>
+                                ))
+                            }
                         </ul>
                     </section>
                     <section id="examples">
