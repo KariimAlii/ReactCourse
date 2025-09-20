67e0cb242fe93843a2463edfadbdd592aa664ad8
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-09-20 12:34:03 +0300

    Jsx is capable of dealing with arrays

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,12 @@ function App() {
                 <main>
                     <section id="core-concepts">
                         <h2>Core Concepts</h2>
+                        {
+                            [
+                                <p>Hello</p>,
+                                <p>World</p>
+                            ]
+                        }
                         <ul>
                             <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description}
                                          image={CORE_CONCEPTS[0].image}/>
